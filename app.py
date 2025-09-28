2d87eea696b776605c6a2536a35a9c29dacc4d4e
Zafira Zain Khan <zafirazainkhan@Zafiras-MacBook-Air.local>
2025-09-28 14:40:46 +0600

    Redesign homepage and update Flask routes for emotional awareness app

--- app.py
+++ app.py
@@ -6,18 +6,21 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html')
 
-@app.route('/brain')
-def brain():
-    return render_template('brain.html')
+@app.route('/Why-am-I-feeling')
+def why_am_i_feeling():
+    return render_template('Why am I feeling?.html')
 
-@app.route('/emotions')
-def emotions():
-    return render_template('emotions.html')
+@app.route('/What-am-I-feeling')
+def what_am_i_feeling():
+    return render_template('What am I feeling?.html')
 
-@app.route('/coping')
-def coping():
-    return render_template('coping.html')
+@app.route('/Mood-Messengers')
+def mood_messengers():
+    return render_template('Mood Messengers.html')
+
+@app.route('/Managing-your-emotions')
+def managing_emotions():
+    return render_template('Managing your emotions.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run(debug=True, port=5000)
